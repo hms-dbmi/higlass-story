0102b1063f7bed168f873a82dbbf0c79b3df5d66
chen650 <chen650@usc.edu>
2018-07-11 13:30:37 -0400

    revert when scrolling up

--- src/editHTML.js
+++ src/editHTML.js
@@ -5,6 +5,8 @@ $(document).ready(function (){
 	var container = document.getElementById('scrollingText');
 	container.appendChild(a);
 
+	activateFunctions = [];
+
 	$("#scrollingText").on('click', '#submitText', function() {
 	  $( "#exampleTextArea" ).submit();
 	});
@@ -26,30 +28,43 @@ $(document).ready(function (){
 					globalVars.json.sections.push(obj);
 					switch(trans[0]) {
 						case "reload":
-							var loadParams = trans[1];
-							globalVars.activateFunctions.push( function() {
-								globalVars.loadHg(loadParams);
+							var loadParams = trans[1].url;
+							activateFunctions.push( function() {
+								globalVars.loadViewConf(loadParams);
 							});
 							break;
 						case "zoom":
 							var zoomParams = trans[1];
-							globalVars.activateFunctions.push( function() {
+							activateFunctions.push( function() {
 								for(var i=0; i<Object.keys(zoomParams).length; i++) {
 									globalVars.hgv.zoomTo(zoomParams[i][0], zoomParams[i][1], zoomParams[i][2], zoomParams[i][3], zoomParams[i][4], 300);
 								}
 							});
 							break;
 						default:
-							globalVars.activateFunctions.push( function() {});
+							activateFunctions.push( function() {});
 					}
 		  	} else {
-		  		globalVars.json.initialHg = globalVars.viewConfUrls[0];
-		  		globalVars.activateFunctions.push( function() {});
-		  		globalVars.json.sections.push({"text": mmd(exampleTextArea.value), "activate": "none", "activateParams": {}});
+		  		var url = globalVars.viewConfUrls[0];
+		  		var initialViewConf = JSON.parse(globalVars.hgv.exportAsViewConfString());
+		  		activateFunctions.push( function() {
+		  			globalVars.loadViewConf(url);
+		  		});
+		  		var obj = {};
+		  		obj.text = mmd(exampleTextArea.value);
+		  		obj.activate = "reload";
+		  		obj.activateParams = {};
+		  		obj.activateParams.url = url;
+          for(var i=0; i<initialViewConf.views.length; i++) {
+	          obj.activateParams[i] = [initialViewConf.views[i].uid, initialViewConf.views[i].initialXDomain[0], initialViewConf.views[i].initialXDomain[1], 
+	          initialViewConf.views[i].initialYDomain[0], initialViewConf.views[i].initialYDomain[1]];
+	        }
+		  		globalVars.json.sections.push(obj);
 		  	}
-		  	scrollerDisplay(d3.select('#graphic'), 'step', globalVars.activateFunctions)
 		  	document.getElementById('exampleTextArea').value = "";
 		  	globalVars.prevViewConf = globalVars.hgv.exportAsViewConfString();
+		  	var reverseFunctions = globalVars.reverse(globalVars.json);
+		  	scrollerDisplay(d3.select('#graphic'), 'step', activateFunctions, reverseFunctions)
 
 				var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(globalVars.json));
 				a.href = 'data:' + data;
